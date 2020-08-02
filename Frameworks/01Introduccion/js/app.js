5519b49a553a0a9a3e40892bb975093798198d03
virgyvaro <badacoin@gmail.com>
2020-08-01 20:49:05 -0500

    Modulo 2

---
+++ Frameworks/01Introduccion/js/app.js
@@ -0,0 +1,32 @@
+ 
+//  Comentario de Línea
+//  Ctrl + }
+/*  Comentario en Bloque*/
+/*  Alt + Shift A */
+
+
+// variable = el valor varia
+// datos texto "texto"
+ var miNombre = "Virgy";
+
+//  num
+var miNum1 = 5;
+var miNum2 = 8;
+
+//  imprimir = mostrar en consola mi nombre
+ console.log(miNombre); 
+
+// console.log(miNum1);
+/* concatenación  adición  */
+
+/* cometario de bloque
+xasdcv
+adfadfad
+*/
+
+// cometario de linea 
+console.log("la suma es :", miNum1 + miNum2);
+
+
+
+
